5ac5bb773ca0ddd928abab728cb2d627c7a28fa3
TribleChief <abhayahire2002@gmail.com>
2021-08-03 20:14:40 +0530

    created card for each member details

---
+++ src/component/card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './main.css'
+
+function Card() {
+    return (
+        <div className='rectangle'>
+            <div className='ellipse'></div>
+            <div className='namecontainer'>Thehhhh Name</div>
+            <div className='socialiconcontainer'>add social icon with the link just as footer</div>
+        </div>
+    )
+}
+
+export default Card
