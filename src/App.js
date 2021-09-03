9db05ff543e8276c1f6b1d22d72c2964a3fe096c
TribleChief <ahire_am@ch.iitr.ac.in>
2021-09-03 19:28:24 +0530

    created the teams page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
-import { Footer } from './component';
+import { Footer } from './component/footer/index.js';
 import { Navigator } from './navigation';
+import {Teams} from './pages/teams-page/index.js'
 function App() {
   return (
     <div className='App'>
-      <Navigator />
+      <Teams/>
+      
     </div>
   );
 }
