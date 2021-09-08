08907b95f34e0193eecf864311b2d93eb624b74c
Ajaya1000 <ajaymati1000@gmail.com>
2021-09-08 14:54:47 +0530

    fixes

--- src/pages/about-page/index.js
+++ src/pages/about-page/index.js
@@ -132,59 +132,6 @@ export const About = () => {
             the support of the majority of students.
           </div>
         </div>
-        <div className={styles.about__description}>
-          <div className={styles.about__heading}>
-            <svg
-              className={styles.preceding__line}
-              width='4'
-              height='35'
-              viewBox='0 0 4 35'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <line x1='2' x2='2' y2='35' stroke='#CF6679' stroke-width='4' />
-            </svg>
-            <h1>Activities of ChESS</h1>
-          </div>
-          <div className={styles.description}>
-            ChESS performs a large number of activities during the whole year.
-            An introduction function for the newly enrolled B.Tech and M.Tech
-            1st year students is one of the most popular event organized by
-            ChESS every year. This function is very auspicious as it serves as a
-            platform to introduce the new students to their seniors and the
-            faculty members .In this way they also learn a lot of new
-            information about their Department. This function is fully managed
-            by the executives of the ChESS
-            <br></br>
-            <br></br>
-            ChESS is engaged in organizing a farewell function for the fourth
-            year students every year. The main motive of this function is to bid
-            farewell to the fourth year students and to greet them good luck for
-            their future.
-            <br></br>
-            <br></br>
-            ChESS maintains a huge library in the department. This organisation
-            consults the faculties and class representative for the requirement
-            of new books and responsible for its procurement in the library. It
-            also organises compaign in which the final year students donate
-            their books in the library for the benefit of upcoming students.
-            <br></br>
-            <br></br>
-            ChESS arranges lecture of distinguished personalities in department
-            of Chemical Engineering. These lectures are meant for increasing the
-            knowledge of students.
-            <br></br>
-            <br></br>
-            ChESS celebrates “OPEN DAY”. On this day students play games such as
-            billiards, table-tennis, chess, carrom. Inter year competitions of
-            outdoor games like cricket, football, basketball, volleyball are
-            also organised. In short it’s a fun day for students.
-            <br></br>
-            <br></br>
-            ChESS arranges debate and quizzes for students. Massive
-            participation is recorded in these events.
-          </div>
-        </div>
       </div>
     </div>
   );
