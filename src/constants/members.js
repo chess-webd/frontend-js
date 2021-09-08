d78ebb67cb231d62d4768201f68a37748f7d61b0
Ajaya1000 <ajaymati1000@gmail.com>
2021-09-08 10:35:52 +0530

    fixes

---
+++ src/constants/members.js
@@ -0,0 +1,19 @@
+const Designation = {
+  FACULTY_ADVISOR: 'Faculty-Advisor',
+  CONVENER: 'Convener',
+  CO_CONVENER: 'Co-Convener',
+  TECHNICAL: 'Technical Team',
+  ALUMINI: 'Alumini Interaction Team',
+  CULTURAL: 'Cultural and Outreach Team',
+};
+
+const Members = [
+  {
+    designation: Designation.FACULTY_ADVISOR,
+    img: '',
+    name: '',
+    fb: '',
+    insta: '',
+  },
+];
+export { Members };
